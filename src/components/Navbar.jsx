5f4cc4a1f9d963c1c5b5c8da4923e29f91da9ef2
Lucky Jha <jhalucky61@gmail.com>
2025-08-16 10:47:15 +0530

    file added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 export default function Navbar({ query, setQuery, onSearch }) {
   return (
-    <div className="flex sm:flex-row flex-col items-center justify-between p-4">
+    <div className="flex sm:flex-row flex-col mb-5 items-center justify-between p-4">
+      <div className="flex flex-row">
+        <img src="public/rupee-sign.svg" alt="rupee sign" className="h-5 sm:h-10 mt-2 mr-2 sm:mt-1" />
       <h1 className="text-2xltext-white sm:text-2xl md:text-3xl lg:text-4xl sm:mb-0 mb-5 font-bold text-gray-800">
         Crypto Price Tracker
       </h1>
+       <img src="public/rupee-sign.svg" alt="rupee sign" className="h-5 sm:h-10 mt-2 ml-2 sm:mt-1" />
+      </div>
     <div className="flex gap-2">
       <input
         type="text"
